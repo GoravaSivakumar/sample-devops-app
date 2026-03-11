5942b41288b3d2ec634ebe5c6d0a6a0f68b6ffde
Sivakumar-streamforce <167304919+SivaStreamForce@users.noreply.github.com>
2026-03-12 02:44:30 +0530

    add github actions pipeline

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hi Siva Kumar, Welcome to DevOps Hands-on Session!"
+    return "Hi Everyone i am done with CICD handson"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
